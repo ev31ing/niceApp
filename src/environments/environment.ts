ac21310d8e98d60ce699dcb83dbcbb73bf5d3f24
ev31ing <evelinag.martin@yandex.ru>
2020-11-14 11:59:29 -0800

    Updated environment file to add Firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyDJ3plS3rxwiV8B6kUZD-bjQlForqU-dZY",
+    authDomain: "niceapp-a483d.firebaseapp.com",
+    databaseURL: "https://niceapp-a483d.firebaseio.com",
+    projectId: "niceapp-a483d",
+    storageBucket: "niceapp-a483d.appspot.com",
+    messagingSenderId: "885353603785",
+    appId: "1:885353603785:web:63c40ff0e95acfe1dd6318",
+    measurementId: "G-89XQD2P5W6"
 };
 
 /*
